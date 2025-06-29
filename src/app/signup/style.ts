382017dc49fdd479f89caf32293f41ae9fd2ecb0
jungyu333 <jungyu3826@gmail.com>
2025-06-29 20:19:44 +0900

    feat : Signup Page Mark up

---
+++ src/app/signup/style.ts
@@ -0,0 +1,14 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  padding-left: 28px;
+  padding-right: 28px;
+`;
